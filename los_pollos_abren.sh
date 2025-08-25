5c289acbf26b61cd2417c0ddcfcdd903b77974f4
José Pérez <jpm70a@gmail.com>
2025-08-25 15:41:51 +0200

    Delete los_pollos_abren.sh

--- los_pollos_abren.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script: los_pollos_abren.sh
-# Añade una línea al log con la fecha y hora del arranque
-
-LOGFILE="/home/fring/los_pollos_abren.log"
-
-echo "Los Pollos Hermanos abre sus puertas el día $(date '+%Y-%m-%d %H:%M:%S')" >> "$LOGFILE"
